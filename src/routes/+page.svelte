55e4853319fc61bacadd3ab54c1260d109d8f87c
john-riordan <iam.john.riordan@gmail.com>
2023-05-30 17:08:58 -0700

    feat: sc hc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,50 +6,92 @@
   export let data = {};
 
   let selectedClass = null;
+  let selectedMode = null;
 
   $: classesLeaderboards = (data.streamers || []).reduce((acc, curr) => {
     if (!acc[curr.class]) acc[curr.class] = [];
     acc[curr.class].push(curr);
     return acc;
-  }, {})
+  }, {});
 
-  $: modeLeaderboards = (data.streamers || []).reduce((acc, curr) => {
-    if (curr.hardcore) acc.hc.push(curr);
-    else acc.sc.push(curr);
-    return acc;
-  }, { hc: [], sc: [] })
+  $: modeLeaderboards = (data.streamers || []).reduce(
+    (acc, curr) => {
+      if (curr.hardcore) acc.hc.push(curr);
+      else acc.sc.push(curr);
+      return acc;
+    },
+    { hc: [], sc: [] }
+  );
 
   $: renderedSteamers = (data.streamers || [])
     .sort((a, b) => b.level - a.level)
     .map((streamer, i) => ({ ...streamer, rankOverall: i }))
-    .filter((streamer) => selectedClass ? streamer.class === selectedClass : streamer);
+    .filter((streamer) => {
+      const playerClass = selectedClass ? streamer.class === selectedClass : true;
+      const mode = streamer.hardcore ? 'hc' : 'sc';
+      const playerMode = selectedMode ? mode === selectedMode : true;
+      return playerClass && playerMode;
+    });
 </script>
 
-
-<nav >
+<nav>
   {#each Object.keys(CLASSES) as classKey}
     {@const className = CLASSES[classKey].name}
     {@const active = selectedClass === classKey}
     <button
       class="serif"
       class:active
-      title="{className}"
+      title={className}
       on:click={() => {
-        if (selectedClass) selectedClass = null
-        else selectedClass = classKey
+        if (selectedClass === classKey) selectedClass = null;
+        else selectedClass = classKey;
       }}
     >
-      <img src="/d4-classes-icon-{classKey}-base.webp" alt="{className}" width="80" height="80" />
-      <img src="/d4-classes-icon-{classKey}-hover.webp" class="hover" alt="{className}" width="80" height="80" />
+      <img
+        src="/d4-classes-icon-{classKey}-base.webp"
+        alt={className}
+        width="80"
+        height="80"
+      />
+      <img
+        src="/d4-classes-icon-{classKey}-hover.webp"
+        class="hover"
+        alt={className}
+        width="80"
+        height="80"
+      />
     </button>
   {/each}
+  <div />
+  <div />
+  <div />
+  <button
+    on:click={() => {
+      if (selectedMode === 'sc') selectedMode = null;
+      else selectedMode = 'sc'
+    }}
+  >
+    Softcore
+  </button>
+  <button
+    on:click={() => {
+      if (selectedMode === 'hc') selectedMode = null;
+      else selectedMode = 'hc'
+    }}
+  >
+    Hardcore
+  </button>
 </nav>
 {#if renderedSteamers.length}
   <ol>
-    {#each renderedSteamers as streamer (streamer.name)}
-      {@const streamerMode = streamer.hardcore ? 'hc' : 'sc'}
-      {@const rankClass = classesLeaderboards[streamer.class].findIndex((s) => s.name === streamer.name)}
-      {@const rankMode = modeLeaderboards[streamerMode].findIndex((s) => s.name === streamer.name)}
+    {#each renderedSteamers as streamer, index (streamer.name)}
+      {@const streamerMode = streamer.hardcore ? "hc" : "sc"}
+      {@const rankClass = classesLeaderboards[streamer.class].findIndex(
+        (s) => s.name === streamer.name
+      )}
+      {@const rankMode = modeLeaderboards[streamerMode].findIndex(
+        (s) => s.name === streamer.name
+      )}
       <li>
         <Streamer
           name={streamer.name}
@@ -71,7 +113,7 @@
 <style lang="scss">
   nav {
     display: flex;
-    gap: 2rem;
+    gap: 0.5rem;
     justify-content: center;
 
     button {
