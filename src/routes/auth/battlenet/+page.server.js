b6fce0e99d69f30bc6fecacb3e68a5c1033618f6
John Riordan <iam.john.riordan@gmail.com>
2023-07-04 17:21:55 -0700

    fix: profile oauth fix

--- src/routes/auth/battlenet/+page.server.js
+++ src/routes/auth/battlenet/+page.server.js
@@ -1,20 +1,19 @@
 import { redirect } from '@sveltejs/kit';
-import crypto from 'crypto';
 
 import {
   PRIVATE_BNET_CLIENT_ID,
   PRIVATE_BNET_CLIENT_SECRET,
 } from '$env/static/private';
 
-const AUTH_URL = 'https://oauth.battle.net/authorize';
+const AUTH_URL = 'https://us.battle.net/oauth/authorize';
 const REDIRECT = 'http://localhost:5173/auth/callback';
 const RESPONSE_TYPE = 'code';
-const SCOPE = '';
+const SCOPE = ' ';
 
 /** @type {import('./$types').PageLoad} */
 export async function load(data) {
   const uuid = data.url.searchParams.get('uuid');
-  const url = `${AUTH_URL}?client_id=${PRIVATE_BNET_CLIENT_ID}&redirect_uri=${REDIRECT}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}&state=${uuid}`;
+  const url = `${AUTH_URL}?client_id=${PRIVATE_BNET_CLIENT_ID}&scope=${SCOPE}&redirect_uri=${REDIRECT}&response_type=${RESPONSE_TYPE}&state=${uuid}`;
 
   throw redirect(307, url);
 }
